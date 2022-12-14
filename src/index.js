e3ba2ee8a63cddf60902757b320e2d70bab619c0
K.J <117104691+K-Jor162@users.noreply.github.com>
2022-12-14 11:29:18 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var name = ["kevin", "josh", "ralph"];
+var i;
+
+for (i = 0; i < 17; i++) {
+  console.log(name);
+  if (i == 16) {
+    console.log("LAST SET MAIN");
+  } else {
+    console.log(name[i]);
+  }
+}
